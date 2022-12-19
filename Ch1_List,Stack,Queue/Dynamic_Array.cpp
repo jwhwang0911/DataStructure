5460fcd70e8635015e779b67e5bea2be0ccff709
jwhwang0911 <star4007lg@gmail.com>
2022-12-19 17:36:34 +0900

    modified:   Ch1_List,Stack,Queue/Dynamic_Array.cpp

--- Ch1_List,Stack,Queue/Dynamic_Array.cpp
+++ Ch1_List,Stack,Queue/Dynamic_Array.cpp
@@ -1,7 +1,7 @@
 #include<string>
 #include<algorithm>
 #include<iostream>
-
+using namespace std;
 template <typename T>
 class dynamic_array {
     T* data;
@@ -25,6 +25,8 @@ class dynamic_array {
             } 
         }
 
+        // & ref 리턴은 해당 값을 변경할 수 있게 하고 싶을때
+        // 데이터구조 연산자는 이런식으로 operator로써 사용할 수 있음
         T& operator[] (int index) {
             return data[index];
         }
@@ -37,9 +39,84 @@ class dynamic_array {
 
         T& at(int index) {
             if (index < n) {
-                if (index < n){
-                    
-                }
+                return data[index];
             }
+            throw "Index out of range";
         }
+
+        size_t size() const {
+            return n;
+        }
+
+        T* begin() {
+            return data;
+        }
+
+        T* end() {
+            return data+n;
+        }
+
+        friend dynamic_array<T> operator+(const dynamic_array<T> &arr1, const dynamic_array<T> &arr2) {
+            dynamic_array<T> result(arr1.size() + arr2.size());
+            copy(arr1.begin(), arr1.end(), result.begin());
+            copy(arr2.begin(), arr2.end(), result.begin()+arr1.size());
+            return result;
+        }   
+
+        string to_string(const string& sep = ",") {
+            if (n==0) {
+                return "";
+            }
+            std::ostringstream os;
+            os << data[0];
+
+            for (int i =1; i < n; i++) {
+                os << sep << data[i];
+            }
+
+            return os.str();
+        }
+
+        ~dynamic_array() {
+            delete[] data;
+        }
+};
+
+struct student{
+    string name;
+    int standard;
+};
+
+ostream& operator<< (ostream& os, const student& s) {
+    return (os << "[" << s.name << "," << s.standard << "]");
+}
+
+int main() {
+    int nStudents;
+    cout << "1반 학생 수 : ";
+    cin >> nStudents;
+
+    dynamic_array<student> class1(nStudents);
+    for(int i = 0; i< nStudents; i++) {
+        string name;
+        int standard;
+
+        cout << i+1 << "번 학생 이름과 나이를 입력하세요 : ";
+        cin >> name >> standard;
+        class1[i] = student{name,standard};
+    }
+
+    try {
+        class1.at(nStudents) = student{"John,18"};
+    } catch(string err) {
+        cout << err << endl;
+    }
+
+    auto class2 = class1;
+    cout << "copy class1 and create class 2 : " << class2.to_string() << endl;
+
+    auto class3 = class1 + class2;
+    cout << "concat class1 and class2 to class3 : " << class3.to_string() << endl;
+
+    return 0;
 }
